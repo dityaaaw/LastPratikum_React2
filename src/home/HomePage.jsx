1ea855e99ffaec59132d8ceb6ad03dadb04b25f1
dityaaaw <144751422+dityaaaw@users.noreply.github.com>
2024-02-03 22:36:29 +0700

    Add files via upload

---
+++ src/home/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HomePage = () => {
+    return (
+        <div>
+            <h1>Selamat Datang Restoran Padang Sederhana</h1>
+            <img src="padang.png"alt="Padang" className="Padang" />
+        </div>
+    )
+}
+
+export default HomePage
